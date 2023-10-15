9bf99eed3fa300e6fca517ebb2178372bd1f86ce
Johannes <Johannes.Rupieper@mac.com>
2023-10-15 15:58:40 +0200

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,197 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const spdColorTheme = {
+  plain: {
+    color: "#E3000F",
+    backgroundColor: "#f6f8fa"
+  },
+  styles: [{
+    types: ["comment", "prolog", "doctype", "cdata"],
+    style: {
+      color: "#999988",
+      fontStyle: "italic"
+    }
+  }, {
+    types: ["namespace"],
+    style: {
+      opacity: 0.7
+    }
+  }, {
+    types: ["string", "attr-value"],
+    style: {
+      color: "#e3116c"
+    }
+  }, {
+    types: ["punctuation", "operator"],
+    style: {
+      color: "#393A34"
+    }
+  }, {
+    types: ["entity", "url", "symbol", "number", "boolean", "variable", "constant", "property", "regex", "inserted"],
+    style: {
+      color: "#36acaa"
+    }
+  }, {
+    types: ["atrule", "keyword", "attr-name", "selector"],
+    style: {
+      color: "#00a4db"
+    }
+  }, {
+    types: ["function", "deleted", "tag"],
+    style: {
+      color: "#d73a49"
+    }
+  }, {
+    types: ["function-variable"],
+    style: {
+      color: "#6f42c1"
+    }
+  }, {
+    types: ["tag", "selector", "keyword"],
+    style: {
+      color: "#00009f"
+    }
+  }]
+};
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Mitte II Alexanderplatz',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.svg',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'de',
+    locales: ['de'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Mitte II Alexanderplatz',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/SPD-Logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'wirSidebar',
+            position: 'left',
+            label: 'Wir',
+          },
+          {
+            type: 'doc',
+            docId: 'aktuelles',
+            position: 'left',
+            label: 'Aktuelles',
+          },
+          {to: '/blog', label: 'Mach mit!', position: 'left'},
+          {
+            type: 'dropdown',
+            label: 'Mehr SPD',
+            position: 'right',
+            items: [
+              {
+                href: 'https://www.spd.de',
+                label: 'Bundesverband',
+              },
+              {
+                href: 'https://www.spd.berlin',
+                label: 'Landesverband Berlin',
+              },
+              {
+                href: 'https://www.spd-berlin-mitte.de',
+                label: 'Berlin-Mitte',
+              }
+            ],
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} SPD Abteilung Mitte II Alexanderplatz.`,
+      },
+      prism: {
+        theme: spdColorTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
